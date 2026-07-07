e52f7a3c8a0c8a33a1ccbb63d03a5caad1809ae3
jegex <hujan008@gmail.com>
2026-07-07 16:40:19 +0700

    change storage to media

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => mb_rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => mb_rtrim(env('APP_URL', 'http://localhost'), '/').'/media',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
